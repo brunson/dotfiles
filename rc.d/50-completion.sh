1c3b0c88ddbd1a2a81ebbaa88dd59d78aa670ff6
Eric Brunson <eric.brunson@oracle.com>
2018-05-25 17:28:56 -0600

    add completion for d()

--- rc.d/50-completion.sh
+++ rc.d/50-completion.sh
@@ -1,3 +1,30 @@
 #!/bin/bash -p
 
 [ -f '/usr/local/bin/aws_bash_completer' ] && source '/usr/local/bin/aws_bash_completer'
+
+_saved_marks()
+{
+    local cur prev opts base
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+
+    opts=$(marks | awk '{ print $1 }')
+
+    case "$COMP_CWORD" in
+	1) COMPREPLY=($(compgen -W "${opts}" -- ${cur}))
+	   return 0
+	   ;;
+	2) local pdir=path_${prev}
+	   local dir=$(eval echo '${'$pdir'}')
+	   local subdirs=$(ls -F $dir | grep /$)
+	   COMPREPLY=($(compgen -W "${subdirs}" -- ${cur}))
+	   return 0
+	   ;;
+    esac
+
+    COMPREPLY=($(compgen -W "${opts}" -- ${cur}))
+    return 0    
+}
+
+complete -F _saved_marks d
