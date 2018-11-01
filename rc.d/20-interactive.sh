c0ea2c087840fb6ede9a59a2c4e8b2d4da83be5d
Eric Brunson <eric.brunson@oracle.com>
2018-11-01 14:35:50 -0600

    from work

--- rc.d/20-interactive.sh
+++ rc.d/20-interactive.sh
@@ -21,8 +21,8 @@ umask 022
 export GOPATH=~
 
 alias olab='sudo openvpn --config ~/.openvpn/ericb.ovpn'
-alias osec='openvpn ~/shared/OS-41975-PWK.ovpn &'
-alias odisco='pkill -TERM openvpn'
+alias osec='sudo openvpn ~/shared/OS-41975-PWK.ovpn &'
+alias odisco='sudo pkill -TERM openvpn'
 alias rdp='rdesktop -g 70% -r disk:bin=/usr/share/windows-binaries -r disk:mydisk=/media/sf_VMShared -u offsec -p cvBwE2P7G3z -a 16 win &'
 alias rdpxl='rdesktop -g 90% -r disk:bin=/usr/share/windows-binaries -r disk:mydisk=/media/sf_VMShared -u offsec -p cvBwE2P7G3z -a 16 win &'
 alias rmkey='ssh-keygen -R'
