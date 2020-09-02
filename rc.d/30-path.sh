993917593c5490afa59b475969ad976ef00f755f
Eric Brunson <eric.brunson@oracle.com>
2020-09-02 16:48:05 -0600

    mess with path

--- rc.d/30-path.sh
+++ rc.d/30-path.sh
@@ -1,5 +1,5 @@
 debug .bash/path.sh
-for dir in /usr/local/sbin /usr/local/sbin /Users/eric.brunson/Library/Python/3.8/bin ~/bin ~/.local/bin ; do
+for dir in /usr/local/sbin /usr/local/sbin /bin ~/.local/bin ; do
     [ -d "$dir" ] && PATH=$dir:$PATH
 done
 
