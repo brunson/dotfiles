8ee6f203237ec595c084399cb5ae49fa7cb0f04e
Eric Brunson <eric.brunson@oracle.com>
2022-05-13 13:42:23 -0600

    take full control over path

--- rc.d/30-path.sh
+++ rc.d/30-path.sh
@@ -1,12 +1,29 @@
 debug .bash/path.sh
 
+BREW=/opt/homebrew
 PYTHON=/Library/Frameworks/Python.framework/Versions/3.10
 export VIRTUALENVWRAPPER_PYTHON=$PYTHON/bin/python3
 
-for dir in /usr/local/bin /usr/local/sbin /opt/homebrew/bin /bin ~/bin ~/.local/bin $PYTHON/bin ; do
+case $(uname -s) in
+    Darwin)
+	DIRS="/bin /usr/bin /sbin /usr/sbin /usr/local/bin $BREW/bin $BREW/opt/coreutils/libexec/gnubin $PYTHON/bin ~/.local/bin ~/bin blarg"
+	;;
+    *)
+	DIRS="/bin /usr/bin /sbin /usr/sbin /usr/local/bin ~/.local/bin ~/bin"
+	;;
+esac
+
+
+NEW_PATH=""
+for dir in $DIRS ; do
     [ -d "$dir" ] || continue
-    echo $PATH | tr : \\n | grep -q ^${dir}$ && continue
-    PATH=$dir:$PATH
+    echo "$NEW_PATH" | tr : \\n | grep -q ^${dir}$ && continue
+    if [[ -z "$NEW_PATH" ]] ; then
+	NEW_PATH=$dir
+    else
+	NEW_PATH=$dir:$NEW_PATH
+    fi
 done
+[[ -z "$NEW_PATH" ]] || PATH=$NEW_PATH
 
 [[ -r "$PYTHON/bin/virtualenvwrapper.sh" ]] && . "$PYTHON/bin/virtualenvwrapper.sh"
