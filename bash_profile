2e076a93a535a57ee7c2640c2ef00579863e081c
Eric Brunson <eric.brunson@oracle.com>
2022-11-16 12:29:39 -0700

    fix path

--- bash_profile
+++ bash_profile
@@ -1,18 +1,3 @@
 # .bash_profile
 source ~/.bashrc
 
-
-# Setting PATH for Python 3.10
-# The original version is saved in .bash_profile.pysave
-PATH="/Library/Frameworks/Python.framework/Versions/3.10/bin:${PATH}"
-export PATH
-
-# Setting PATH for Python 3.11
-# The original version is saved in .bash_profile.pysave
-PATH="/Library/Frameworks/Python.framework/Versions/3.11/bin:${PATH}"
-export PATH
-
-# Setting PATH for Python 3.10
-# The original version is saved in .bash_profile.pysave
-PATH="/Library/Frameworks/Python.framework/Versions/3.10/bin:${PATH}"
-export PATH
--- rc.d/30-path.sh
+++ rc.d/30-path.sh
@@ -8,25 +8,32 @@ export VIRTUALENVWRAPPER_PYTHON=$PYTHON/bin/python3
 
 case $(/usr/bin/uname -s) in
     Darwin)
-	DIRS="$GOPATH/bin /bin /usr/bin /sbin /usr/sbin /usr/local/bin $HOME/.rd/bin $BREW/bin $BREW/opt/coreutils/libexec/gnubin $MYPYTHON/bin $PYTHON/bin $HOME/.local/bin $HOME/bin"
-	;;
+        debug Darwin
+        DIRS="$GOPATH/bin /bin /usr/bin /sbin /usr/sbin /usr/local/bin $HOME/.rd/bin $BREW/bin $BREW/opt/coreutils/libexec/gnubin $MYPYTHON/bin $PYTHON/bin $HOME/.local/bin $HOME/bin"
+        ;;
     *)
-	DIRS="/bin /usr/bin /sbin /usr/sbin /usr/local/bin ~/.local/bin ~/bin"
-	;;
+        debug not Darwin
+        DIRS="/bin /usr/bin /sbin /usr/sbin /usr/local/bin $HOME/.local/bin $HOME/bin"
+        ;;
 esac
 
 export PATH=/bin:/usr/bin
 
 NEW_PATH=""
 for dir in $DIRS ; do
+    debug process $dir
     [ -d "$dir" ] || continue
     echo "$NEW_PATH" | /usr/bin/tr : \\n | /usr/bin/grep -q ^${dir}$ && continue
+    debug add $dir
     if [[ -z "$NEW_PATH" ]] ; then
-	NEW_PATH=$dir
+        NEW_PATH=$dir
     else
-	NEW_PATH=$dir:$NEW_PATH
+        NEW_PATH=$dir:$NEW_PATH
     fi
 done
+debug NEW_PATH=$NEW_PATH
+debug PATH=$PATH
 [[ -z "$NEW_PATH" ]] || PATH=$NEW_PATH
+debug PATH=$PATH
 
 [[ -r "$PYTHON/bin/virtualenvwrapper.sh" ]] && . "$PYTHON/bin/virtualenvwrapper.sh"
