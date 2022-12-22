0c0712530ff7e693f3eb73e3c36cdc9063285f77
Eric Brunson <eric.brunson@oracle.com>
2022-12-22 12:01:16 -0700

    detect ssh-agent

--- rc.d/60-ssh-agent.sh
+++ rc.d/60-ssh-agent.sh
@@ -1,7 +1,14 @@
-
-DIR=~/.ssh
-
-for key in id_rsa id_rsa_gitlab id_rsa_github; do
-    [ -f $DIR/$key ] && ssh-add -l | grep -qw $key || ssh-add $DIR/$key 2>&- | grep -sv "Identity added:" || :
-done
+ssh-add -l >&- 2>&-
+if [[ $? -eq 2 ]] ; then
+    echo no ssh agent
+else
+    # keep JuiceSSH agent from hanging
+    ssh-add -l | grep -q JuiceSSH
+    if [[ $? -ne 0 ]] ; then
+	DIR=~/.ssh
+	for key in id_rsa id_rsa_gitlab id_rsa_github; do
+	    [ -f $DIR/$key ] && ssh-add -l | grep -qw $key || echo adding $key ; ssh-add $DIR/$key 2>&- | grep -sv "Identity added:" || :
+	done
+    fi
+fi
 
