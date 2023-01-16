d56b88d9426aa716bffd84eca5015be7f7801f15
Eric Brunson <eric.brunson@oracle.com>
2023-01-16 15:58:40 -0700

    fix up ssh-agent script to not interfere with sftp

--- rc.d/60-ssh-agent.sh
+++ rc.d/60-ssh-agent.sh
@@ -1,13 +1,13 @@
 ssh-add -l >&- 2>&-
-if [[ $? -eq 2 ]] ; then
-    echo no ssh agent
-else
+if [[ $? -ne 2 ]] ; then
     # keep JuiceSSH agent from hanging
     ssh-add -l | grep -q JuiceSSH
     if [[ $? -ne 0 ]] ; then
 	DIR=~/.ssh
 	for key in id_rsa id_rsa_gitlab id_rsa_github; do
-	    [ -f $DIR/$key ] && ssh-add -l | grep -qw $key || echo adding $key ; ssh-add $DIR/$key 2>&- | grep -sv "Identity added:" || :
+	    if [ -f $DIR/$key ] ; then
+		ssh-add -l | grep -qw $key || echo adding $key >&2 ; ssh-add $DIR/$key 2>&- | grep -sv "Identity added:" || :
+	    fi
 	done
     fi
 fi
